d79e3f3f9547bf42b768f2e980734d1faa5755a7
busreservation99 <bus.reservation99@gmail.com>
2021-04-19 13:34:20 +0530

    Bus Fetch Module on Home Page Working - MRS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
+import { SearchBusService } from './../search-bus.service';
+import { SearchBus } from './../appmodel/searchBus';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor() { }
+  searchBus:SearchBus= new SearchBus();
+  
+  message:String;
 
-  ngOnInit(): void {
+  data:any;
+  
+  
+  constructor(private searchBusService:SearchBusService, private router: Router) { }
+  fetchBus(){
+
+    
+    this.searchBusService.fetchBus(this.searchBus).subscribe(response => {
+      // alert(JSON.stringify(response));
+      console.log(response);
+      this.data=response;
+    })
   }
+  }
+   
+    
 
-}
