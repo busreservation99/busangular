1744d554bbdc341f08fa424862026b55a9bf4405
busreservation99 <bus.reservation99@gmail.com>
2021-04-25 17:20:45 +0530

    Seats Confirmation and Booking - Mihir Sanghrajka

--- src/app/seats/seats.component.ts
+++ src/app/seats/seats.component.ts
@@ -1,7 +1,9 @@
+import { UpdateSeatService } from './../update-seat.service';
 import { PassingSeat } from './../appmodel/PassingSeat';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FetchSeatStatusService } from '../fetch-seat-status.service';
+
 
 @Component({
   selector: 'app-seats',
@@ -10,6 +12,8 @@ import { FetchSeatStatusService } from '../fetch-seat-status.service';
 })
 export class SeatsComponent implements OnInit {
    
+  bookedSeatId:number[]=new Array();
+
   totalPrice:number=null;
   
   totalSeatsBooked:number=null;
@@ -33,7 +37,7 @@ export class SeatsComponent implements OnInit {
   sid;
   
   passingseat : PassingSeat = new PassingSeat();
-  constructor(private router: Router,private fetchSeatStatusService:FetchSeatStatusService) { }
+  constructor(private router: Router,private fetchSeatStatusService:FetchSeatStatusService,private updateSeats:UpdateSeatService) { }
   ngOnInit(): void {   
     this.busno=sessionStorage.getItem('busNumber');
     this.sid=sessionStorage.getItem('schid');
@@ -45,6 +49,7 @@ export class SeatsComponent implements OnInit {
   fetchSeatStatus(){
       this.fetchSeatStatusService.fetchSeatStatusBus(this.busno,this.sid).subscribe(response => {
       this.seatStatus=response;
+      console.log(this.seatStatus);
           for (let i = 0; i <this.seatStatus.length; i++) {
             this.seats.push(i);   
           }    
@@ -74,15 +79,26 @@ faltufn(){
       console.log(this.seatsBooked);
       this.totalPrice=this.seatsBooked.length*this.price;
       this.totalSeatsBooked=this.seatsBooked.length;
+      sessionStorage.setItem('totalPrice',this.totalPrice.toString());
       console.log(" Total Price="+this.totalPrice+" Total seats booked= "+this.totalSeatsBooked.toString());
 
 }
 
 confirmBooking(){
   this.router.navigate(['passenger']);
-  sessionStorage.setItem('seatsBooked',JSON.stringify(this.seatsBooked));
+  console.log(this.seatsBooked.toString())
+  sessionStorage.setItem('seatsBooked',JSON.stringify(this.seatsBooked.toString()));
   sessionStorage.setItem('totalSeats',JSON.stringify(this.seatsBooked.length));
-  this.passingseat.seatconfirm=this.seatsBooked;
+  this.seatsBooked.forEach(element => {
+    this.updateSeats.UpdateSeats(element).subscribe(data =>{
 
+      this.bookedSeatId.push(data[0].seatId);
+      
+       sessionStorage.setItem('seatId',this.bookedSeatId.toString());
+      alert(JSON.stringify(data));
+      console.log(data);
+     });
+  });
+  
 }
 }
