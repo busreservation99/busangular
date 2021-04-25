45d3cc7581ebcf0cc2180bca140f2a4da33713f9
shreya <sbhagole1998@gmail.com>
2021-04-25 11:38:27 +0530

    Login & Registration component Rectified Shreya

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Login } from '../appmodel/login';
+import { Registration } from '../appmodel/Registration';
 import { RegisterService } from '../register.service';
 
 @Component({
@@ -15,11 +16,21 @@ export class LoginComponent  {
   
   constructor(private registerService:RegisterService, private router: Router) { }
 
+  registration: Registration  = new Registration();
+  message1:string;
+  
+
+  addCustomer(){
+    this.registerService.saveCustomer(this.registration).subscribe(data =>{
+      alert(JSON.stringify(data));
+      this.message1=data['message'];
+    }) 
+  }
   loginCheck() {
     console.log(this.login);
     this.registerService.login(this.login).subscribe(response => {
-      alert(JSON.stringify(response));
-      console.log(response);
+      //alert(JSON.stringify(response));
+      //console.log(response);
       if(response.status == true) {
         let registrationId = response.registrationId;
         let customerName = response.name;
